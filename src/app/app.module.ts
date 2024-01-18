79b327fa299e51d7ef0be443bc8977a1b0d53c82
diegov67 <vargas.diego@ficct.uagrm.edu.bo>
2024-01-18 17:20:34 -0400

    First section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { HighlightComponent } from './components/highlight/highlight.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    HighlightComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
